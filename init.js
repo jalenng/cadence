c2e484a914b96a11501d11fbe204d3df8685340a
jalenng <jalen_ng@outlook.com>
2020-08-07 21:43:37 -0700

    implement about dialog

--- init.js
+++ init.js
@@ -3,6 +3,28 @@ var WindowButton = require('./windowbtn.js');
 var openAboutWindow = require('about-window').default;
 var join = require('path').join;
 
+// about dialog
+function createAboutDialog() {
+	let win = new BrowserWindow({
+		width: 1280,
+		height: 800,
+		minWidth: 720,
+		minHeight: 800,
+		backgroundColor: '#2e2c29',
+		frame: false,
+		enableRemoteModule: true,
+		icon: "ico/AZCad.ico",
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
+
+	win.loadFile('index.html');
+	
+	// Open the DevTools
+	win.webContents.openDevTools();
+}
+
 const menuTemplate = [
 	{
 		role: 'fileMenu',
@@ -70,18 +92,26 @@ const menuTemplate = [
 		role: 'help',
 		submenu: [
 			{
-				label: "About Cadence",
+				label: 'About Cadence',
 				click() {openAboutWindow({
+					win_options: {
+						parent: currentWindow,
+						width: 640,
+						height: 360,
+						resizable: false,
+						fullscreenable: false,
+						modal: true,
+						frame: false
+					},
 					icon_path: join(__dirname, 'ico/AZCad.ico'),
 					package_json_dir: __dirname,
-					css_path: [
-						join(__dirname, 'styles/about-styles.css')
-					]
+					about_page_dir: __dirname,
+					show_close_button: 'Close'
 				});}
 			},
 			{label: 'Check for Updates'}
 		]
 	}
 ]
-
+MediaControls.update();
 Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
